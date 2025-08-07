dd09776fa08ab12e6de57b21e89b1d4d902f6dd8
DimasMahardika24 <109904745+DimasMahardika24@users.noreply.github.com>
2025-08-07 22:29:21 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+  API_KEY: 'ptla_qKEyRhjZscMVVCp6EdFdque6zBLAtDlXkuaCluSkgl2',
+  PANEL_URL: 'https://panel-leveo.panelfast.my.id',
+  EGG_ID: 18,           // ID Egg default (misal Node.js = 1)
+  NEST_ID: 5,          // ID Nest default (misal Software = 1)
+  ALLOCATION_ID: 1     // ID Allocation aktif di lokasi tertentu
+};
